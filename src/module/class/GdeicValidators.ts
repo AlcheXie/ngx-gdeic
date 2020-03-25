933cf69c5062f015ed8bf5502331924fdef5f687
AlcheXie <alchemize47ys@hotmail.com>
2020-03-25 09:44:57 +0800

    feat(class: Validators): add max and min validator

--- src/module/class/GdeicValidators.ts
+++ src/module/class/GdeicValidators.ts
@@ -1,8 +1,23 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
+// @dynamic
 export class GdeicValidators {
   static required(control: AbstractControl): ValidationErrors | null {
     const _value = control.value;
     return (_value === null || _value === undefined || _value === '' || _value.length === 0) ? { 'required': true } : null;
+  }
+
+  static max(max: Number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const _value = control.value;
+      return _value > max ? { 'maxValue': max } : null;
+    };
+  }
+
+  static min(min: Number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const _value = control.value;
+      return _value < min ? { 'minValue': min } : null;
+    };
   }
 }
